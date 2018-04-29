180d6849a567a83f17109bd7a78e537a107efe4e
jnyenhuis <34197935+jnyenhuis@users.noreply.github.com>
2018-04-28 20:52:24 -0500

    initial commit

---
+++ openglviewportwidget.h
@@ -0,0 +1,27 @@
+#ifndef OPENGLVIEWPORTWIDGET_H
+#define OPENGLVIEWPORTWIDGET_H
+
+#include <QWidget>
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_3_3_Core>
+
+
+class OpenGLViewportWidget : public QOpenGLWidget
+{
+public:
+    QOpenGLFunctions_3_3_Core *glFunctions;
+    uint vertexShader;
+    uint fragmentShader;
+    explicit OpenGLViewportWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+
+protected:
+    void initializeGL();
+    void resizeGL(int w, int h);
+    void paintGL();
+};
+
+#endif // OPENGLVIEWPORTWIDGET_H
