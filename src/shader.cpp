94c39931e961b4b096ea3634a774136f19e6d4de
jnyenhuis <jnyenhuis17@gmail.com>
2018-05-02 14:08:28 -0500

    create and load shaders from files

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,6 +1,52 @@
 #include "shader.h"
 
-Shader::Shader()
-{
+#include "fileio.h"
+#include "globals.h"
 
+#include <QDebug>
+
+Shader::Shader(QOpenGLFunctions_3_3_Core *gl, std::string vertShaderPath, std::string fragShaderPath)
+{
+    // Generate the shader program and shader IDs.
+    shaderProgramId = gl->glCreateProgram();
+
+    // Attach vertex shader.
+    vertexShaderId = gl->glCreateShader(GL_VERTEX_SHADER);
+    loadShaderSource(vertShaderPath, GL_VERTEX_SHADER, gl);
+    gl->glAttachShader(shaderProgramId, vertexShaderId);
+
+    // Attach fragment shader.
+    fragmentShaderId = gl->glCreateShader(GL_FRAGMENT_SHADER);
+    loadShaderSource(fragShaderPath, GL_FRAGMENT_SHADER, gl);
+    gl->glAttachShader(shaderProgramId, fragmentShaderId);
+
+    gl->glLinkProgram(shaderProgramId);
+}
+
+bool Shader::loadShaderSource(std::string filename, GLenum shaderType, QOpenGLFunctions_3_3_Core *gl)
+{
+    // Read shader text from file. OpenGL can't use std::string directly.
+    std::string shaderText = OpenGlModelViewer::readAllText(filename);
+    if (shaderText == "") {
+        return false; // File wasn't read correctly or is empty.
+    }
+    const char *shaderSource = shaderText.c_str();
+
+    uint shaderId;
+    if (shaderType == vertexShaderId) {
+        shaderId = vertexShaderId;
+    } else if (shaderType == GL_FRAGMENT_SHADER) {
+        shaderId = fragmentShaderId;
+    } else {
+        return false; // Shader type is not supported.
+    }
+
+    // Load and compile the shader source.
+    gl->glShaderSource(shaderId, 1, &shaderSource, nullptr);
+    gl->glCompileShader(fragmentShaderId);
+
+    // Check if the shader compiled correctly.
+    GLint compiledSuccessfully;
+    gl->glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &compiledSuccessfully);
+    return compiledSuccessfully > 0;
 }
--- src/shader.h
+++ src/shader.h
@@ -1,11 +1,20 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include <QOpenGLFunctions_3_3_Core>
+#include <string>
 
 class Shader
 {
 public:
-    Shader();
+    Shader(QOpenGLFunctions_3_3_Core *gl, std::string vertShaderPath, std::string fragShaderPath);
+
+private:
+    uint shaderProgramId;
+    uint fragmentShaderId;
+    uint vertexShaderId;
+
+    bool loadShaderSource(std::string filename, GLenum shaderType, QOpenGLFunctions_3_3_Core *gl);
 };
 
 #endif // SHADER_H
